4c4f33160028ab42c4fe5c76ee6e15c34873a785
patrickgue <me@patrickguenthard.ch>
2019-05-12 19:25:14 +0200

    adapted gui to editor

--- html/app/app.js
+++ html/app/app.js
@@ -1,8 +1,12 @@
-const pdfjs = require("pdfjs");
-const remote = require('electron').remote; 
+const remote = require('electron').remote;
 const $ = require("jquery");
 
-$(function () {
+$(function() {
+    let docu = new E(document.getElementById("document"), testDoc);
+
+    $("#pos-settings").hide();
+    $("#text-settings").hide();
+
     $("#max-btn").click(function() {
         const window = remote.getCurrentWindow();
         if (!window.isMaximized()) {
@@ -21,4 +25,40 @@ $(function () {
         const window = remote.getCurrentWindow();
         window.close();
     });
+
+    $("#undo-btn").click(docu.undo);
+
+    docu.addSelectionEvent(function(elm) {
+        $("#text-settings").hide();
+        $("#pos-settings").hide();
+
+        $("#pos-settings").show();
+        $("#edit-x-inp").val(elm.pos.x);
+        $("#edit-y-inp").val(elm.pos.y);
+        $("#edit-w-inp").val(elm.pos.width);
+        $("#edit-h-inp").val(elm.pos.height);
+        if (elm.type == E.type.text) {
+            $("#text-settings").show();
+            $("#edit-text-inp").val(elm.text);
+        }
+    });
+
+    docu.addUnselectEvent(function() {
+        $("#text-settings").hide();
+        $("#pos-settings").hide();
+    });
+
+    $("#edit-x-inp, #edit-y-inp, #edit-w-inp, #edit-h-inp").on("change", function() {
+        docu.selectElm.pos.x = parseInt($("#edit-x-inp").val());
+        docu.selectElm.pos.y = parseInt($("#edit-y-inp").val());
+        docu.selectElm.pos.width = parseInt($("#edit-w-inp").val());
+        docu.selectElm.pos.height = parseInt($("#edit-h-inp").val());
+        docu.render();
+    });
+
+    $("#edit-text-inp").on("change", function() {
+        docu.selectElm.text = $("#edit-text-inp").val();
+        docu.render();
+    });
+
 });
